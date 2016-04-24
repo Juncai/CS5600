1895980655fda1334bc6bc4472634c21fa5f3629
Juncai <cai.jun@husky.neu.edu>
2016-04-24 08:22:28 -0400

    start debugging extra

---
+++ extra/master.c
@@ -0,0 +1,288 @@
+#include "master.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/select.h>
+#include <sys/time.h>
+#include <sys/poll.h>
+#include <sys/epoll.h>
+
+#define MAXEVEDNTS 1000
+void computeExponential(char *worker_path, char *mechanism, int x, int n);
+
+int main(int argc, char *argv[])
+{
+	if (argc != 9) {
+		printf("Usage: --worker_path WORKER_PATH --wait_mechanism MECHANISM -x X -n N\n");
+		return -1;
+	}
+
+	char *worker_path = argv[2];
+	char *mechanism = argv[4];
+	int x = atoi(argv[6]);
+	int n = atoi(argv[8]);
+
+	computeExponential(worker_path, mechanism, x, n);
+	return 0;
+}
+
+void computeExponential(char *worker_path, char *mechanism, int x, int n)
+{
+	int i;
+	int status = 0;
+	char x_str[50];
+	char n_str[50];
+	int cid = -1;
+	char i_str[50];
+	char w_str[50];
+	int pipefds[2];
+	char buf;
+	int exec_res;
+	char *argv[6];
+
+	sprintf(x_str, "%d", x);
+	sprintf(n_str, "%d", n);
+
+	argv[0] = "./worker";
+	argv[1] = "-x";
+	argv[2] = x_str;
+	argv[3] = "-n";
+	argv[4] = i_str;
+	argv[5] = (char *) NULL;
+
+	if (strcmp(mechanism, "sequential") == 0) {
+		for (i = 0; i <= n; i++) {
+			sprintf(i_str, "%d", i);
+			pipe(pipefds);
+			cid = fork();
+			if (cid == 0) {
+				close(pipefds[0]);
+				dup2(pipefds[1], STDOUT_FILENO);
+				exec_res = execv(worker_path, argv);
+				if (exec_res == -1) perror("exec failed");
+				_exit(EXIT_FAILURE);
+			} else {
+				waitpid(cid, &status, 0);
+				close(pipefds[1]);
+				sprintf(w_str, "worker %d: ", i);
+				write(STDOUT_FILENO, w_str, strlen(w_str));
+				while (read(pipefds[0], &buf, 1) > 0)
+                   write(STDOUT_FILENO, &buf, 1);
+				close(pipefds[0]);
+			}
+		}
+	}
+
+	if (strcmp(mechanism, "select") == 0) {
+		// create a list of pipe read-ends
+		fd_set rfds;
+		int read_ends[n + 1];
+		int write_ends[n + 1];
+		int max_fd = 0;
+
+		FD_ZERO(&rfds);
+		
+		for (i = 0; i <= n; i++) {
+			pipe(pipefds);
+			FD_SET(pipefds[0], &rfds);
+			read_ends[i] = pipefds[0];
+			write_ends[i] = pipefds[1];
+			if (pipefds[0] > max_fd) {
+				max_fd = pipefds[0];
+			}
+		}
+
+		for (i = 0; i <= n; i++) {
+			sprintf(i_str, "%d", i);
+			cid = fork();
+			if (cid == 0) {
+				close(read_ends[i]);
+				dup2(write_ends[i], STDOUT_FILENO);
+				exec_res = execv(worker_path, argv);
+				if (exec_res == -1) perror("exec failed");
+				_exit(EXIT_FAILURE);
+			} else {
+				close(write_ends[i]);
+			}
+		}
+
+		struct timeval tv;
+		int retval;
+		int counter = n + 1;
+
+		while (counter > 0) {
+		/* while (1) { */
+			// reset the rfds
+			FD_ZERO(&rfds);
+			for (i = 0; i <= n; i++) {
+				if (read_ends[i] != 0) {
+					FD_SET(read_ends[i], &rfds);
+				}
+			}
+			// reset the timer
+			// wait for half a second
+			tv.tv_sec = 0;
+			tv.tv_usec = 500000;
+			retval = select(max_fd + 1, &rfds, NULL, NULL, &tv);
+
+			if (retval == -1) {
+				perror("select");
+				return;
+			} else if (retval) {
+				for (i = 0; i <= n; i++) {
+					if (FD_ISSET(read_ends[i], &rfds)) {
+						sprintf(w_str, "worker %d: ", i);
+						write(STDOUT_FILENO, w_str, strlen(w_str));
+						while (read(read_ends[i], &buf, 1) > 0)
+						    write(STDOUT_FILENO, &buf, 1);
+						close(read_ends[i]);
+						read_ends[i] = 0;
+						counter--;
+					}
+				}
+			}
+		}
+	}
+
+	if (strcmp(mechanism, "poll") == 0) {
+		// create a list of pipe read-ends
+		struct pollfd pfds[n + 1];
+		int read_ends[n + 1];
+		int write_ends[n + 1];
+		int max_fd = 0;
+		int timeout = 500;  // in milisecond
+		
+		for (i = 0; i <= n; i++) {
+			pipe(pipefds);
+			pfds[i].fd = pipefds[0];
+			pfds[i].events = POLLIN;
+			read_ends[i] = pipefds[0];
+			write_ends[i] = pipefds[1];
+			if (pipefds[0] > max_fd) {
+				max_fd = pipefds[0];
+			}
+		}
+
+		for (i = 0; i <= n; i++) {
+			sprintf(i_str, "%d", i);
+			cid = fork();
+			if (cid == 0) {
+				close(read_ends[i]);
+				dup2(write_ends[i], STDOUT_FILENO);
+				exec_res = execv(worker_path, argv);
+				if (exec_res == -1) perror("exec failed");
+				_exit(EXIT_FAILURE);
+			} else {
+				close(write_ends[i]);
+			}
+		}
+
+		int retval;
+		int counter = n + 1;
+
+		// wait for half a second
+		while (counter > 0) {
+			retval = poll(pfds, n + 1, timeout);
+
+			if (retval == -1) {
+				perror("poll");
+				return;
+			} else if (retval) {
+				for (i = 0; i <= n; i++) {
+					if (pfds[i].revents & POLLIN) {
+						sprintf(w_str, "worker %d: ", i);
+						write(STDOUT_FILENO, w_str, strlen(w_str));
+						while (read(read_ends[i], &buf, 1) > 0)
+						    write(STDOUT_FILENO, &buf, 1);
+						close(read_ends[i]);
+						read_ends[i] = 0;
+						counter--;
+					}
+				}
+			}
+		}
+	}
+
+	if (strcmp(mechanism, "epoll") == 0) {
+		// create a list of pipe read-ends
+		struct epoll_event ev, events[MAXEVEDNTS];
+		int epollfd;
+		int read_end, write_end;
+		int read_ends[n + 1];
+		int write_ends[n + 1];
+		int max_fd = 0;
+		int retval;
+		int counter = n + 1;
+		
+		// create epoll instance
+		epollfd = epoll_create1(0);
+		if (epollfd == -1) {
+			perror("epoll");
+			exit(EXIT_FAILURE);
+		}
+
+		for (i = 0; i <= n; i++) {
+			pipe(pipefds);
+			read_end = pipefds[0];
+			write_end = pipefds[1];
+			// add event to epoll
+			ev.data.fd = read_end;
+			ev.events = EPOLLIN | EPOLLET;
+			retval = epoll_ctl(epollfd, EPOLL_CTL_ADD, read_end, &ev);
+			if (retval == -1) {
+				perror ("epoll_ctl");
+				exit(EXIT_FAILURE);
+			}
+			// store the read and write end fds
+			read_ends[i] = read_end;
+			write_ends[i] = write_end;
+
+			if (pipefds[0] > max_fd) {
+				max_fd = pipefds[0];
+			}
+		}
+
+		// create threads for executing worker program
+		for (i = 0; i <= n; i++) {
+			sprintf(i_str, "%d", i);
+			cid = fork();
+			if (cid == 0) {
+				close(read_ends[i]);
+				dup2(write_ends[i], STDOUT_FILENO);
+				exec_res = execv(worker_path, argv);
+				if (exec_res == -1) perror("exec failed");
+				_exit(EXIT_FAILURE);
+			} else {
+				close(write_ends[i]);
+			}
+		}
+
+		while (counter > 0) {
+			int valid_fd = 0;
+			int n_eve, i_eve;
+			n_eve = epoll_wait(epollfd, events, MAXEVEDNTS, -1);
+			if (n_eve == 0) break;
+			for (i_eve = 0; i_eve < n_eve; i_eve++) {
+				read_end = events[i_eve].data.fd;
+				for (i = 0; i < n + 1; i++) {
+					if (read_ends[i] == read_end) {
+						sprintf(w_str, "worker %d: ", i);
+						write(STDOUT_FILENO, w_str, strlen(w_str));
+						valid_fd = 1;
+						read_ends[i] = -1;
+						counter--;
+					}
+				}
+				if (valid_fd) {
+					while (read(read_end, &buf, 1) > 0)
+						write(STDOUT_FILENO, &buf, 1);
+				}
+				close(read_end);
+			}
+		}
+	}
+	return;
+}
---
+++ extra/mtexponential.c
@@ -0,0 +1,135 @@
+#include "worker.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+static pthread_mutex_t taskLock;
+static pthread_mutex_t outputLock;
+static sem_t taskSem;
+static sem_t outputSem;
+
+double compute(int x, int n);
+void *WorkThread();
+
+struct node {
+	int x;
+	int n;
+	double res;
+	pthread_t tid;
+	struct node *next;
+} *taskQHead, *outputQHead;
+
+int main(int argc, char *argv[])
+{
+	int ret;
+	double res = 0;
+	void *status;
+	if (argc != 7) {
+		printf("Usage: ./mtexponential -x X -n N -t T\n");
+		return 0;
+	}
+	int x = atoi(argv[2]);
+	int n = atoi(argv[4]);
+	int t = atoi(argv[6]);
+
+	// init semaphore
+	ret = pthread_mutex_init(&taskLock, NULL);
+	ret = pthread_mutex_init(&outputLock, NULL);
+	ret = sem_init(&taskSem, 0, 0);
+	ret = sem_init(&outputSem, 0, 0);
+	
+	// create thread pool
+	pthread_t pool[t];
+	int i;
+	for (i = 0; i < t; i++) {
+		ret = pthread_create(&pool[i], NULL, WorkThread, NULL);
+	}
+
+	// init queues
+	struct node taskHead;
+	taskQHead = &taskHead;
+	struct node outputHead;
+	outputQHead = &outputHead;
+	
+	// create task queue
+	for (i = 0; i <= n; i++) {
+		pthread_mutex_lock(&taskLock);
+		struct node newTask;
+		newTask.x = x;
+		newTask.n = i;
+		newTask.next = taskQHead->next;
+		taskQHead->next = &newTask;
+		pthread_mutex_unlock(&taskLock);
+		sem_post(&taskSem);
+	}
+		
+	
+	// print out all results
+	for (i = 0; i <= n; i++) {
+		sem_wait(&outputSem);
+		pthread_mutex_lock(&outputLock);
+		struct node *cOutput = outputQHead->next;
+		outputQHead->next = outputQHead->next->next;
+		pthread_mutex_unlock(&outputLock);
+		res += cOutput->res;
+		printf("%d^%d / %d! : %f. From thread %zu\n", x, cOutput->n, cOutput->n, cOutput->res, cOutput->tid);
+	}
+
+	// kill all threads
+	for (i = 0; i < t; i++) {
+		/* pthread_kill(pool[i], 9); */
+		pthread_cancel(pool[i]);
+		pthread_join(pool[i], &status);
+	}
+
+	printf("SUM(%d^n / n!) = %f\n", x, res);
+
+	pthread_exit(NULL);
+}
+
+void *WorkThread()
+{
+	while(1) {
+		sem_wait(&taskSem);
+		pthread_mutex_lock(&taskLock);
+		struct node *cTask = taskQHead->next;
+		taskQHead->next = taskQHead->next->next;
+		pthread_mutex_unlock(&taskLock);
+		/* struct node cRes; */
+		/* cRes.n = cTask->n; */
+		/* cRes.x = cTask->x; */
+		/* cRes.res = compute(cRes.x, cRes.n); */
+		cTask->res = compute(cTask->x, cTask->n);
+		cTask->tid = pthread_self();
+		pthread_mutex_lock(&outputLock);
+		cTask->next = outputQHead->next;
+		outputQHead->next = cTask;
+		/* cRes.next = outputQHead->next; */
+		/* outputQHead->next = &cRes; */
+		pthread_mutex_unlock(&outputLock);
+		sem_post(&outputSem);
+	}
+
+}
+
+double compute(int x, int n)
+{
+	if (n == 0) return 1;
+	int n_f = 1;
+	int i;
+
+	for (i = 1; i <= n; i++) {
+		n_f *= i;
+	}
+
+	double res = pow(x, n) / n_f;
+
+	return res;
+}
+
+
