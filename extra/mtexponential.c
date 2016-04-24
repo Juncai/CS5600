1b2afd5d4c85d485cee85845ab67859c66c902b3
Juncai <cai.jun@husky.neu.edu>
2016-04-24 18:50:09 -0400

    continue

--- extra/mtexponential.c
+++ extra/mtexponential.c
@@ -21,7 +21,7 @@ struct node {
 	double res;
 	pthread_t tid;
 	struct node *next;
-} *taskQHead, *outputQHead;
+} taskQHead, outputQHead, *tmpNode;
 
 int main(int argc, char *argv[])
 {
@@ -35,34 +35,41 @@ int main(int argc, char *argv[])
 	int x = atoi(argv[2]);
 	int n = atoi(argv[4]);
 	int t = atoi(argv[6]);
+	int i;
 
 	// init semaphore
 	ret = pthread_mutex_init(&taskLock, NULL);
 	ret = pthread_mutex_init(&outputLock, NULL);
 	ret = sem_init(&taskSem, 0, 0);
+	/* ret = sem_init(&taskSem, 0, -1); */
 	ret = sem_init(&outputSem, 0, 0);
+	/* ret = sem_init(&outputSem, 0, -1); */
 	
 	// create thread pool
 	pthread_t pool[t];
-	int i;
 	for (i = 0; i < t; i++) {
 		ret = pthread_create(&pool[i], NULL, WorkThread, NULL);
 	}
 
 	// init queues
-	struct node taskHead;
-	taskQHead = &taskHead;
-	struct node outputHead;
-	outputQHead = &outputHead;
+	/* struct node taskHead; */
+	/* taskQHead = &taskHead; */
+	/* struct node outputHead; */
+	/* outputQHead = &outputHead; */
 	
 	// create task queue
 	for (i = 0; i <= n; i++) {
 		pthread_mutex_lock(&taskLock);
-		struct node newTask;
-		newTask.x = x;
-		newTask.n = i;
-		newTask.next = taskQHead->next;
-		taskQHead->next = &newTask;
+		tmpNode = (struct node *)malloc(sizeof(struct node));
+		tmpNode->x = x;
+		tmpNode->n = i;
+		tmpNode->next = NULL;
+		taskQHead.next = tmpNode;
+		/* struct node newTask; */
+		/* newTask.x = x; */
+		/* newTask.n = i; */
+		/* newTask.next = taskQHead.next; */
+		/* taskQHead.next = &newTask; */
 		pthread_mutex_unlock(&taskLock);
 		sem_post(&taskSem);
 	}
@@ -72,11 +79,12 @@ int main(int argc, char *argv[])
 	for (i = 0; i <= n; i++) {
 		sem_wait(&outputSem);
 		pthread_mutex_lock(&outputLock);
-		struct node *cOutput = outputQHead->next;
-		outputQHead->next = outputQHead->next->next;
+		struct node *cOutput = outputQHead.next;
+		outputQHead.next = outputQHead.next->next;
 		pthread_mutex_unlock(&outputLock);
 		res += cOutput->res;
 		printf("%d^%d / %d! : %f. From thread %zu\n", x, cOutput->n, cOutput->n, cOutput->res, cOutput->tid);
+		free(cOutput);
 	}
 
 	// kill all threads
@@ -96,8 +104,12 @@ void *WorkThread()
 	while(1) {
 		sem_wait(&taskSem);
 		pthread_mutex_lock(&taskLock);
-		struct node *cTask = taskQHead->next;
-		taskQHead->next = taskQHead->next->next;
+		struct node *cTask = taskQHead.next;
+		if (cTask == NULL) {
+			pthread_mutex_unlock(&taskLock);
+			continue;
+		}
+		taskQHead.next = taskQHead.next->next;
 		pthread_mutex_unlock(&taskLock);
 		/* struct node cRes; */
 		/* cRes.n = cTask->n; */
@@ -106,8 +118,8 @@ void *WorkThread()
 		cTask->res = compute(cTask->x, cTask->n);
 		cTask->tid = pthread_self();
 		pthread_mutex_lock(&outputLock);
-		cTask->next = outputQHead->next;
-		outputQHead->next = cTask;
+		cTask->next = outputQHead.next;
+		outputQHead.next = cTask;
 		/* cRes.next = outputQHead->next; */
 		/* outputQHead->next = &cRes; */
 		pthread_mutex_unlock(&outputLock);
